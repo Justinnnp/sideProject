921c750ad60e74aafbc1fe1f89f2294550d9a66d
Justin Plein <2099449@talnet.nl>
2020-05-20 14:14:10 +0200

    een opzet met een inlog/uitlog/register page.

---
+++ signup.php
@@ -0,0 +1,14 @@
+<?php
+require 'header.php';
+?>
+
+<main>
+    <h1>Signup</h1>
+    <form action = 'includes/signup.inc.php' method = 'post'>
+
+    </form>
+</main>
+
+<?php
+require 'footer.php';
+?>
